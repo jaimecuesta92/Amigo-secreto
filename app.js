2c3c981c3d47695b895fd780c452c27ec598d006
Jaime Cuesta <jaimeandres.cuesta@gmail.com>
2025-08-06 14:25:10 -0500

    Se agrega función de agregar los nombres a listas en HTML

--- app.js
+++ app.js
@@ -7,16 +7,32 @@ console.log(listaDeAmigos);
 function agregarAmigo (){
     let nombre = document.getElementById("amigo").value.trim();
 
-    if (nombre == ""){
+    if (nombre == "" || !isNaN(nombre)){
         alert("Por favor ingresa un nombre");
     }else{
         listaDeAmigos.push(nombre);
     }
     console.log(listaDeAmigos);
 limpiarCaja();
+agregarAmigosALista();
 }
 
 function limpiarCaja(){
     document.querySelector("#amigo").value="";
     return;
 }
+
+//Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. 
+//Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+function agregarAmigosALista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+for (let i = 0; i < listaDeAmigos.length; i++) {
+    let elemento = document.createElement("li");
+    elemento.textContent = listaDeAmigos[i];
+    //Agrega el elemento a la lista.
+    lista.appendChild(elemento);
+    }
+
+}
+
