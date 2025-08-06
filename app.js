52f800fcfd64c94ecb8c8a60144c29248e36c5f6
Jaime Cuesta <jaimeandres.cuesta@gmail.com>
2025-08-06 10:30:29 -0500

    Se implementa función para agregar amigos, se incluye la limpieza del campo de entrada

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaDeAmigos = [];
+
+console.log(listaDeAmigos);
+function agregarAmigo (){
+    let nombre = document.getElementById("amigo").value.trim();
+
+    if (nombre == ""){
+        alert("Por favor ingresa un nombre");
+    }else{
+        listaDeAmigos.push(nombre);
+    }
+    console.log(listaDeAmigos);
+limpiarCaja();
+}
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value="";
+    return;
+}
